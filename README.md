7fbb45f5b4536f1ff869ab21b9b713e7312cdd52
TomY8s <Thomas.Yates@bglgroup.co.uk>
2018-06-14 15:35:52 +0100

    app v 0.0.1

---
+++ README.md
@@ -0,0 +1,32 @@
+Dinner Party Menu Generator (DPMG)
+=
+
+Are you planning a posh dinner party, but lack the inspiration to create your own menu?
+
+DO NOT PANIC! DPMG can help!
+
+How to use
+==
+
+Simply clone the repo
+
+```
+git clone git@github.com:Tomy8s/dinner_party_menu_generator.git
+```
+
+Move into the cloned directory
+
+```
+cd dinner_party_menu_generator
+```
+
+Run the file!
+
+```
+ruby main.rb
+```
+
+Dependencies
+==
+
+- Ruby
---
+++ main.rb
@@ -0,0 +1,34 @@
+require 'json'
+
+class MenuGenerator
+    def initialize
+        ask_options
+    end
+
+    def ask_options
+        print 'How sophisticated would you like your meals to be (how many indregients are you prepared to use)? '
+        complexity = check_input_num gets.chomp
+        puts "Why not try #{generate_dish(complexity).join(' ')}?"
+    end
+
+    def check_input_num input
+        input = input.to_i
+        if input == 0 then
+            puts "Please enter a number greater than 0"
+            return ask_options
+        end
+        input
+    end
+
+
+    def generate_dish complexity
+        json = File.read './foods.json'
+        foods = JSON.parse json
+        if complexity > foods.size
+            complexity = foods.size
+        end
+        foods.shuffle.pop complexity 
+    end
+end
+
+dish = MenuGenerator.new
